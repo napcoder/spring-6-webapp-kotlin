3a6a98b2b3740e5db17ce07984ac196a99ee7a0c
Marco Travaglini <marco.travaglini@cloudacademy.com>
2023-09-08 21:47:25 +0200

    chore: initial commit

---
+++ src/main/kotlin/it/napcoder/spring6webappkotlin/Spring6WebappKotlinApplication.kt
@@ -0,0 +1,11 @@
+package it.napcoder.spring6webappkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Spring6WebappKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<Spring6WebappKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/it/napcoder/spring6webappkotlin/Spring6WebappKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.napcoder.spring6webappkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Spring6WebappKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
